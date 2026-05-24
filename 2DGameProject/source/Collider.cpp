bdae9eb321b3a4b61a14548c9e5939888da9bdd5
fuky <fuky2131neco@gmail.com>
2026-05-24 23:13:15 +0900

    円と矩形の判定の追加

--- 2DGameProject/source/Collider.cpp
+++ 2DGameProject/source/Collider.cpp
@@ -5,9 +5,9 @@ Collider::Collider() :m_position(0, 0), m_width(0), m_height(0), m_radius(0), m_
 {
 }
 
-bool Collider::IsHit(const Collider& other)
+bool Collider::IsHit(const Collider& other) const
 {
-	//どちらもBoxの場合
+	//どちらも矩形の場合
 	if (m_type == ColliderType::Box && other.m_type == ColliderType::Box)
 	{
 		//MYRECTへ変換
@@ -24,8 +24,9 @@ bool Collider::IsHit(const Collider& other)
 		b.h = (int)other.m_height;
 
 		//当たり判定を返す
-		return IsHitBox(a.x, a.y, a.w, a.h, b.x, b.y, b.w, b.h);
+		return IsHitBox(a,b);
 	}
+	//どちらも円の場合
 	if (m_type == ColliderType::Circle && other.m_type == ColliderType::Circle)
 	{
 		//中心点距離
@@ -34,6 +35,27 @@ bool Collider::IsHit(const Collider& other)
 		float totalRadius = m_radius + other.m_radius;
 
 		return distance <= totalRadius;
+	}
+	//自身が円、対象が矩形の場合
+	if (m_type == ColliderType::Circle && other.m_type == ColliderType::Box)
+	{
+		MYCIRCLE c;
+		c.x = m_position.x;
+		c.y = m_position.y;
+		c.r = m_radius;
+
+		MYRECT r;
+		r.x = (int)other.m_position.x;
+		r.y = (int)other.m_position.y;
+		r.w = (int)other.m_width;
+		r.h = (int)other.m_height;
+
+		return IsHitCircleBox(c, r);
+	}
+	//逆の場合
+	if (m_type == ColliderType::Box && other.m_type == ColliderType::Circle)
+	{
+		return other.IsHit(*this);
 	}
 	return false;
 }
